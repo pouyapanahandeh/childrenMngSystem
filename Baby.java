a0ab73ae7c4a328e07392ffc9c555e190a9a0f33
pooya <pooya.panahandeh@hotmail.com>
2017-10-24 16:40:26 +0200

    javaHw

--- Baby.java
+++ Baby.java
@@ -4,6 +4,10 @@ public class Baby{
 
 	public static void main(String[] args){
 	
+	BabyDet objOne = new BabyDet();
+	Time    objTwo = new Time();
+	GiftDet objThree = new GiftDet();
+
 	System.out.println("Welcome to Baby  Management System");
 	System.out.println("please select one of the option : ");
 
