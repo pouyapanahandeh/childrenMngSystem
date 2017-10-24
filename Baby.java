ca8f9860048132c8ca66fe8ecc58aa44c74f2329
pooya <pooya.panahandeh@hotmail.com>
2017-10-24 17:00:58 +0200

    javaHw

--- Baby.java
+++ Baby.java
@@ -7,10 +7,26 @@ public class Baby{
 	BabyDet objOne = new BabyDet();
 	Time    objTwo = new Time();
 	GiftDet objThree = new GiftDet();
+		
+
 
 	System.out.println("Welcome to Baby  Management System");
 	System.out.println("please select one of the option : ");
+	
+	
+	int i = 1, j = 2, k = 3, q = 4;
+	int opt = 0;
 
+	switch (opt){
+
+	case 1 : System.out.println("Hi"); break;
+	case 2 : System.out.println("Hi Hi"); break;
+	case 3 : System.out.println("Hi Hi Hi"); break;
+	case 4 : System.out.println("Hi Hi Hi Hi"); break;
+
+	default : System.out.println("You entered wrong character"); break; 
+	
+	}
 
 
 
