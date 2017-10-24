2c2b962e6a58e4e6d587d83b88cd2d06187bbbaf
pooya <pooya.panahandeh@hotmail.com>
2017-10-24 16:36:15 +0200

    javaHw

--- Baby.java
+++ Baby.java
@@ -4,7 +4,8 @@ public class Baby{
 
 	public static void main(String[] args){
 	
-	System.out.println("pooya panahandeh s7rfbi");
+	System.out.println("Welcome to Baby  Management System");
+	System.out.println("please select one of the option : ");
 
 
 
