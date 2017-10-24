311d255af8163877bc0b0448cd28b9524c43669d
pooya <pooya.panahandeh@hotmail.com>
2017-10-24 13:38:13 +0200

    javaHw

---
+++ Time.java
@@ -0,0 +1,17 @@
+import java.util.Date;
+
+public class Time{
+	
+	/* modifying time for each addition */
+		
+	private Date modTime;
+	
+	public Date getModTime(){
+		return this.modTime;
+	}
+
+	public void setModTime(Date modTime){
+		this.modTime = modTime;
+	}
+	
+} 
