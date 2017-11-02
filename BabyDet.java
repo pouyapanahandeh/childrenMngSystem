0b08cffb07ea1f4ef0382773cc7c6bd364e0756b
pooya <pooya.panahandeh@hotmail.com>
2017-11-02 19:39:43 +0100

    Java Hw

--- BabyDet.java
+++ BabyDet.java
@@ -3,24 +3,7 @@ import java.text.*;
 
 public class BabyDet{
 
-	private String name;
-	private int age;
-	
+	private static ArrayList<BabyNo> BabyList = new ArrayList<>();
 
-	public String getName(){
-		return this.name;
-	}
-
-	public void setName(String name){
-		this.name = name;
-	}
-
-	public int getAge(){
-		return this.age;
-	}
-
-	public void setAge(int age){
-		this.age = age;
-	}
-
+	public static String listOfBaby(){
 }
