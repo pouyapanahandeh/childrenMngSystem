26dba28782d626c960f28458b1bbe15109c51b59
pooya <pooya.panahandeh@hotmail.com>
2017-11-02 14:01:27 +0100

    Java Hw

--- GiftDet.java
+++ GiftDet.java
@@ -3,9 +3,13 @@ import java.text.*;
 
 public class GiftDet{
 
-	private String giftDet;
 	private String giftGiver;
+	private String giftDet;
 	private Date dateDet;
+	i
+	private SimpleDateFormat dayForm = new SimpleDateFormat("dd/MM/yyyy");
 
+	//constructor 
+	public GiftDet
 }
 
