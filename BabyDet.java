88edebef4bfa8d2e440f89287120e461547f5151
pooya <pooya.panahandeh@hotmail.com>
2017-11-03 10:53:24 +0100

    Java Hw

--- BabyDet.java
+++ BabyDet.java
@@ -8,6 +8,21 @@ public class BabyDet{
 	private String babyName;
 	private int babyAge;
 
+	public String getBabyName(){
+		return babyName;
+	}
+
+	public void setBabyName(String babyName){
+		this.babyName = babyName;
+	}
+
+	public int getBabyAge(){
+		return babyAge;
+	}
+
+	public void setBabyAge(int babyAge){
+		this.babyAge = babyAge;
+	}
 
 }
 	
