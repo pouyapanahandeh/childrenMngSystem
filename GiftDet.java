bfabd808530a278ba80f11829759e4b915768c3b
pooya <pooya.panahandeh@hotmail.com>
2017-11-03 18:31:31 +0100

    Java Hw

--- GiftDet.java
+++ GiftDet.java
@@ -2,20 +2,38 @@ import java.util.*;
 import java.text.*;
 
 public class GiftDet{
-
-	private String giftGiver;
-	private String giftDet;
-	private Date dateDet;
 	
-	private SimpleDateFormat dayForm = new SimpleDateFormat("dd/MM/yyyy");
+	private ArrayList<Babydet> gifts;
+
+	//private String giftGiver;
+	//private String giftDet;
+	//private Date dateDet;
+	
+	//private SimpleDateFormat dayForm = new SimpleDateFormat("dd/MM/yyyy");
 
 	//constructor 
-	public GiftDet(String giftGiver,String giftDet,Date date){
-		
-		this.giftGiver = giftGiver;
-		this.giftDet = giftDet;
-		this.dateDet = dateDet;		
-	
+	//public GiftDet(String giftGiver,String giftDet,Date date){
+	//	
+	//	this.giftGiver = giftGiver;
+	//	this.giftDet = giftDet;
+	//	this.dateDet = dateDet;		
+	//
+	//}
+
+	public void setGifts(ArrayList<BabyDet> gifts){
+		this.gifts = gifts;
+	}
+
+	public GiftDet(){
+		gifts = new ArrayList<BabyDet>();
+	}
+
+	public void addGifts(BabyDet g){
+		gifts.add(g);
+	}
+
+	public int theNoGifts(){
+		return gifts.size();
 	}
  
 	public GiftDet() {
