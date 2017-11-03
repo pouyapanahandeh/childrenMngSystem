9da947d74dfe6210fda10bab12a29dd9bde66e04
pooya <pooya.panahandeh@hotmail.com>
2017-11-03 10:44:11 +0100

    Java Hw

--- Baby.java
+++ Baby.java
@@ -5,23 +5,32 @@ public class Baby{
 
 	public static void main(String[] args){
 	
-	BabyDet objOne = new BabyDet();
-	GiftDet objThree = new GiftDet();
+//	BabyDet objOne = new BabyDet();
+//	GiftDet objThree = new GiftDet();
+
 
 	List<String> BabyList = new ArrayList<String>();
 	Scanner userInput = new Scanner(System.in);
 		
-
+		
 
 	System.out.println("Welcome to Baby  Management System");
-	System.out.println("please select one of the option : ");
 	
-	System.out.println("1-Add new Baby." + "/n" +
-	 "2-List of Babies by Age" + "/n" +
-	 "3-List of Babies by Name" + "/n" +
-	 "4-Add new Gift" + "/n" +
-	 "5-List of Gifts" + "/n");	
+	
+	
+	System.out.println("1-Add new Baby." + "\n" +
+	 "2-List of Babies by Age" + "\n" +
+	 "3-List of Babies by Name" + "\n" +
+	 "4-Add new Gift" + "\n" +
+	 "5-List of Gifts" + "\n");	
 
+	System.out.println("please enter your choice : ");
+	int selector = userInput.nextInt(); 	
 
+	if(selector == 1){
+		System.out.println("Please enter Babys Name : ");
+			
 	}
+
+    }
 }
